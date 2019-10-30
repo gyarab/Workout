672454a8f84508fd3c1201b54364bf805f31bdd7
unknown <dominik.patzak@student.gyarab.cz>
2019-10-30 12:59:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/workout/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.workout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            val intent = Intent(this@MainActivity, chooseProgram::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
